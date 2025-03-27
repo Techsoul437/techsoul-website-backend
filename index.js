7c10459ad7ca0c5d523495affda1077bef49642e
DishantC <dishantch7@gmail.com>
2025-03-27 14:49:20 +0530

    deploy issue fix -1

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 8000;
-const MONGOURL = process.env.MONGO_URL || 'mongodb://localhost:27017/TechsoulSolutions';
+const MONGOURL = process.env.MONGO_URL;
 
 app.use(bodyParser.json());
 app.use(cors());
